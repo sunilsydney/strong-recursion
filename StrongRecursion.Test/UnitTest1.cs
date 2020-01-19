273ccc92c3bd3f87b5bb8b8da91c8a2e97cbc8e9
Sunil Purushothaman <sunilmsdn@outlook.com>
2020-01-19 11:39:36 +1100

    Initial commit

---
+++ StrongRecursion.Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StrongRecursion.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            int n = 5000;
+            int expected = (n * (n + 1)) / 2;
+
+            int actual = Sum(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private int Sum(int n)
+        {
+            // sum = 1 + 2 + 3 + ..... + n
+
+            // Limiting condition
+            if( n == 1)
+            {
+                return 1;
+            }
+
+            return n + Sum(n - 1);
+        }
+    }
+}
---
+++ StrongRecursion/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StrongRecursion
+{
+    public class Class1
+    {
+    }
+}
